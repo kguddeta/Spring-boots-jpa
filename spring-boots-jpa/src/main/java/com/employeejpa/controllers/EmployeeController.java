ca5f4c0248e6215138d7a233ea83b3f2f2c2374a
kgudd <kgudd@192.168.0.19>
2018-10-06 22:07:34 -0500

    Employee Controller, Repository

--- spring-boots-jpa/src/main/java/com/employeejpa/controllers/EmployeeController.java
+++ spring-boots-jpa/src/main/java/com/employeejpa/controllers/EmployeeController.java
@@ -3,7 +3,10 @@ package com.employeejpa.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.employeejpa.model.Employee;
 import com.employeejpa.respository.EmployeeRepository;
 
+
 @RestController
 public class EmployeeController {
 	
@@ -19,13 +23,14 @@ public class EmployeeController {
 	private EmployeeRepository er;
 	
 	@RequestMapping(value="/employees", method = RequestMethod.GET)
-	public List<Employee> getEmployees(){
-		return (List<Employee>)er.findAll();
+	public List<Employee> getAllEmployees(){
+		return er.findAll();
 	}
 
 	@RequestMapping(value="/employees/{id}", method = RequestMethod.GET)
-	public Optional<Employee> getEmployees(Long id){
-		return er.findById(id);
+	public Optional<Employee> getEmployee(@PathVariable Long id){		
+			return er.findById(id);
+		
 	}
 	
 	@RequestMapping(value="/employees", method = RequestMethod.POST)
@@ -33,5 +38,17 @@ public class EmployeeController {
 			er.save(employee);
 		return employee;
 	}	
+	
+	
+	@RequestMapping(value="/employees/{id}", method = RequestMethod.PUT)
+	public Employee update(@PathVariable Long id, @RequestBody Employee employee) {
+		er.save(employee);
+		return employee;
+	}
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable Long id) {
+		er.deleteById(id);
+		return "Succesfully deleted!";
+	}
 	
 }
