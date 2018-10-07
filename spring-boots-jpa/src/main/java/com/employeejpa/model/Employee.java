8c64c0afa672603b340143fd516d359361be508e
kgudd <kgudd@192.168.0.19>
2018-10-06 19:17:54 -0500

    toString()

--- spring-boots-jpa/src/main/java/com/employeejpa/model/Employee.java
+++ spring-boots-jpa/src/main/java/com/employeejpa/model/Employee.java
@@ -48,53 +48,12 @@ public class Employee {
 		this.lastName = lastName;
 		this.salary = salary;
 	}
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private Integer salary;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public Integer getSalary() {
-		return salary;
-	}
-	public void setSalary(Integer salary) {
-		this.salary = salary;
-	}
-	
-	public Employee() {
-		
-	}
-	public Employee(Long id, String firstName, String lastName, Integer salary) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.salary = salary;
-	}
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
 				+ "]";
 	}
-	
+
 	
 	
 }
